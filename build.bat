065b01ce443b012c2086328efb36e40ab6019128
torbatti <torbattii@gmail.com>
2025-09-16 00:22:19 +0330

    add: better file api interface

--- build.bat
+++ build.bat
@@ -10,7 +10,14 @@ set release_exe_name=sanam
 set build_debug= 1
 set build_release= 0
 
-:: ----------------------------------------------------------------------------
+:: available debugger procs | raddbg is default
+set debug_raddbg= 1
+set debug_gdb= 1
+set debug_ldb= 1
+
+:: available formatter procs | clang-fmt is default
+set clang_fmt= 1
+
 :: available compilers | zig is default
 set zig= 1
 
@@ -18,19 +25,26 @@ set zig= 1
 :: General Paths
 set tooling_path= C:\dev\tooling
 set zig_path=%tooling_path%\zig-x86_64-windows-0.14.1
+set llvm_path=%tooling_path%\llvm-mingw-20250709-ucrt-x86_64
 
 :: Compilers path
 set zig_exe=%zig_path%\zig.exe
 
+:: Debuggers path
+
+:: Formatters path
+set clang_fmt_exe=%llvm_path%\bin\clang-format.exe
+
 :: ----------------------------------------------------------------------------
 :: General Options
-set general_options= -std=c89
+set general_options= -std=c99
 
 :: Linker options
 set link_options=
 
 :: Debug options -g -Wall -Wextra -pedantic -Wshadow -Werror -fPIC -fPIE
-if %build_debug% == 1 set debug_options= -g -Wall -Wextra -pedantic -Wshadow -fPIC -fPIE
+if %build_debug% == 1 set debug_options= -g 
+:: if %build_debug% == 1 set debug_options= -g -Wall -Wextra -pedantic -Wshadow -fPIC -fPIE
 
 :: Release options
 if %build_release% == 1 set release_options= -O2 -fPIC -fPIE
--- src/sanam.c
+++ src/sanam.c
@@ -30,12 +30,161 @@
 /*
  * DOCUMENTATION:
  *
+ *
+ *
+ *
+ *
+ * planned platforms:
+ *   [ ] x86_64-windows
+ *   [ ] x86_64-linux
+ *   [ ] arm_64-linux
+ *   [ ] x86_64-macos
+ *   [ ] arm_64-macos
+ *   [ ] x86_64-openbsd
+ *   [ ] x86_64-netbsd
+ *   [ ] x86_64-freebsd
+ *
  */
 
 char SANAM_VERSION[] = "UNTRACKED";
 
 /*
- * File / Directory
+ * Os specific Includes
+ *
+ *
+ *
+ */
+
+#ifdef _WIN32 /* Both 32 bit and 64 bit */
+
+#  include <Windows.h>
+#  include <stdio.h>
+
+#endif /* _WIN32 */
+
+#ifdef __linux__
+#endif /* __linux__ */
+
+#ifdef __MACH__
+#endif /* __MACH__ */
+
+/*
+ * Memmory related
+ *
+ */
+
+/*
+static int
+mem_alloc(void *mem_pointer, unsigned int mem_size)
+{
+
+#ifdef _WIN32
+  DWORD registerclass_err;
+
+  mem_pointer = VirtualAlloc(0, mem_size, MEM_COMMIT, PAGE_READWRITE);
+
+  if(!mem_pointer)
+  {
+    registerclass_err = GetLastError();
+    return -1;
+  }
+
+  return 0;
+#endif
+}
+*/
+
+/*
+static int
+mem_clear()
+{}
+*/
+
+/*
+static int
+mem_free(void *mem_pointer)
+{
+  VirtualFree(mem_pointer, 0, MEM_RELEASE);
+  return 0;
+}
+*/
+
+/*
+ * File related
+ *
+ */
+
+static int
+file_open(FILE **ptrfile, const char *name, const char *mode)
+{
+  *ptrfile = fopen(name, mode);
+  if(ptrfile == NULL)
+  {
+    return -1;
+  }
+  return 0;
+}
+
+/* NOTE(AABIB): better api interface i guess? */
+static int
+file_readByte(FILE *ptrfile)
+{
+  int fgetc_resault = fgetc(ptrfile);
+  return fgetc_resault;
+}
+
+/* TODO(AABIB): Better errors handling */
+static long
+file_readBytes(FILE *ptrfile, char *ptrbuff, int max_read)
+{
+  long startpos = ftell(ptrfile);
+  if(startpos == -1)
+  {
+    return -1;
+  }
+
+  char *fgets_resault = fgets(ptrbuff, max_read, ptrfile);
+  /* TODO(AABIB): Handle errors */
+  /* fgets_resault == null and errno is set */
+  /*
+  // if(!fgets_resault)
+  // {
+  //   return -1;
+  // }
+  */
+
+  long endpos = ftell(ptrfile);
+  if(endpos == -1)
+  {
+    return -1;
+  }
+
+  /* + 1 is not needed , endpos hasnt been read yet*/
+  long read_len = endpos - startpos;
+  return read_len;
+}
+
+/*
+static int
+file_read()
+{}
+*/
+
+static int
+file_close(FILE *ptrfile)
+{
+  int fclose_resault = fclose(ptrfile);
+
+  /* returns 0 on success*/
+  if(fclose_resault == 0)
+  {
+    return 0;
+  }
+  return -1;
+}
+
+/*
+ * Directory related
  *
  */
 
@@ -44,6 +193,7 @@ char SANAM_VERSION[] = "UNTRACKED";
  *
  * backends: poll
  *
+ *  NOTE(AABIB): have no idea how to implement it on windows ffs
  */
 
 /*
@@ -115,7 +265,7 @@ utf8_readChar(utf8_Reader *reader)
         utf8_rune_len = 4;
       }
 
-      /* ASSERT(AABIB): This shouldnt have happened */
+      /* NOTE(AABIB): This shouldnt have happened */
       if(utf8_rune_len == 0)
       {
         return_rune_err.rune = 0;
@@ -177,21 +327,36 @@ struct md_Token {
 int
 main(int argc, char *argv[])
 {
-  char  filename[] = "test.md";
-  FILE *file       = fopen("test.md", "r");
-  if(file == NULL)
+  char buff[2048];
+
+  FILE *file        = NULL;
+  char  file_name[] = "test.md";
+  char  file_mode[] = "r";
+
+  int fileopen_resault = file_open(&file, "test.md", file_mode);
+
+  if(fileopen_resault == -1)
   {
-    printf("Unable to open file %s\n", filename);
-    return 1;
+    printf("Unable to open file %s\n", file_name);
+    return -1;
   }
 
-  /*
-  // char buffer[2048];
-  // int  count = 0;
-  // count      = pread(file, buffer, 2048, 0);
-  */
+  while(1)
+  {
+    char *fgets_resault = fgets(buff, 2048, file);
+    if(fgets_resault == NULL)
+    {
+      break;
+    }
+    printf("%s", buff);
+  }
 
-  fclose(file);
+  int fileclose_resault = file_close(file);
+  if(fileopen_resault == -1)
+  {
+    printf("Unable to close file %s\n", file_name);
+    return -1;
+  }
 
   return 0;
 }
